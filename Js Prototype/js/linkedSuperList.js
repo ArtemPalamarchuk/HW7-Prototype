7b0db69210474b469345fa3a67c98212295d2caf
Foks991 <33574193+Foks991@users.noreply.github.com>
2019-10-30 13:28:10 +0200

    lList reverse

--- Js Prototype/js/linkedSuperList.js
+++ Js Prototype/js/linkedSuperList.js
@@ -7,6 +7,23 @@ LinkedSuperList.prototype.constructor = LinkedSuperList;
 
 LinkedSuperList.prototype.map = function() {};
 LinkedSuperList.prototype.reduce = function() {};
-LinkedSuperList.prototype.reverse = function() {};
+LinkedSuperList.prototype.reverse = function() {
+    let tmp = this.root;
+    let flag = this.root.next.next;
+
+    while (flag.next !=null){
+        while(tmp.next !== null){
+            tmp = tmp.next;
+        }
+    }
+    return "return"
+};
 
 const linkedSuperList = new LinkedSuperList();
+linkedSuperList.push(1);
+linkedSuperList.push(2);
+linkedSuperList.push(3);
+linkedSuperList.push(4);
+linkedSuperList.push(5);
+
+
