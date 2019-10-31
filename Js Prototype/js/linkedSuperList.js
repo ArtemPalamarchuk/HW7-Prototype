db940ad48e2620b0bf1781c490c7b1ab4f50d09e
Foks991 <33574193+Foks991@users.noreply.github.com>
2019-11-01 01:47:36 +0200

    HW 7

--- Js Prototype/js/linkedSuperList.js
+++ Js Prototype/js/linkedSuperList.js
@@ -5,10 +5,31 @@ function LinkedSuperList() {
 LinkedSuperList.prototype = Object.create(LinkedList.prototype);
 LinkedSuperList.prototype.constructor = LinkedSuperList;
 
-LinkedSuperList.prototype.map = function() {
-
-};
-LinkedSuperList.prototype.reduce = function() {};
+LinkedSuperList.prototype.map = function (f) {
+    if (typeof f != "function") return this;
+    const newList = new LinkedSuperList();
+    let tmp = this.root;
+    let i = 0;
+    while (tmp.next) {
+        tmp = tmp.next;
+        newList.push(f(tmp.el, i, LinkedSuperList));
+        i++;
+    }
+    return newList;
+}
+LinkedSuperList.prototype.reduce = function (f, initValue) {
+    if (typeof f != "function") return 0;
+    let result = 0;
+    if (initValue) result = initValue;
+    let tmp = this.root;
+    let i = 0;
+    while (tmp.next && tmp.next != null) {
+        tmp = tmp.next;
+        result = f(result, tmp.el, i, LinkedSuperList)
+        i++;
+    }
+    return result;
+}
 LinkedSuperList.prototype.reverse = function() {
 
     let a = [];
